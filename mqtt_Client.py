bc75ed0ba17c517c810ebbe1b15a90993c20762a
Juan Pablo Rivera Ruiz <a01636350@tec.mx>
2022-06-04 19:12:43 +0200

    Added missing starting files

---
+++ mqtt_Client.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+import uuid, random, string
+
+class MQTT_Client(mqtt.Client):
+    def __init__(self):
+        # Creating a unique client id
+        self.id = self.__generate_client_id()
+        # Default broker address
+        self.broker_address = "test.mosquitto.org"
+        # Creating client object with the id
+        super().__init__(self.id)
+
+        # Binding callback functions
+        self.on_connect = self.__on_connect
+        self.on_disconnect = self.__on_disconnect
+        self.on_message = self.__on_message
+
+    def connect(self, broker_address=""):
+        if len(broker_address) != 0:
+            self.broker_address = broker_address
+
+        super().connect(self.broker_address)
+
+    def __generate_client_id(self):
+        # Making a client_id 23 characters long
+        mac = str(hex(uuid.getnode()))[2:]
+        random_str = "".join(random.choice(string.ascii_letters) for _ in range(11))
+        return mac + random_str
+
+    def __on_message(self, client, userdata, msg):
+        topic = msg.topic
+        m_decode = str(msg.payload.decode("utf-8", "ignore"))
+        print("recieved message text:", m_decode)
+
+    def __on_connect(self, client, userdata, flags, rc):
+        if rc:
+            print(f"A problem occured, could not connect to the broker: {self.broker_address}")
+        else:
+            print(f"Succesfully connected to broker: {self.broker_address}")
+
+    def __on_disconnect(self, client, userdata, flags, rc=0):
+        if rc:
+            print(f"A problem occured, could not disconnect from the broker: {self.broker_address}")
+        else:
+            print(f"Succesfully disconnected from broker: {self.broker_address}")
---
+++ smartCar.py
@@ -0,0 +1,179 @@
+from mqtt_Client import MQTT_Client
+from piDriver import PiDriver
+from piCamera import PiCamera
+from us_Sensor import US_Sensor
+import cv2
+import signal
+import threading
+import time
+import numpy as np
+
+class SmartCar:
+    def __init__(self):
+        self.client = MQTT_Client()
+        self.driver = PiDriver()
+        self.camera = PiCamera()
+        self.us_sensor = US_Sensor(5,6)
+        self.params = { 'help':False,
+                        'debug':False,
+                        'capture':False }
+
+    def debug_mode(self):
+        self.params['debug'] = True
+        self.driver.debug = True
+        self.camera.debug = True
+        self.us_sensor.debug = False
+
+    def start(self):
+        # Initiating MQTT client as well as loop
+        #self.client.connect()
+        #self.client.loop_start()
+        
+        # Create deamon thread for reading ultrasonic_sensor
+        threading.Thread(target=self.us_sensor.start, daemon=True).start()
+        
+        try:
+            while(self.camera.cap.isOpened()):
+                # Checking there's no risk of colission
+                if self.us_sensor.distance < 6:
+                    if not self.colission:
+                        self.driver.stop()
+                        self.colission = True
+                elif self.us_sensor.distance > 10:
+                    self.colission = False
+                
+                # Obtaining frame
+                ret, frame = self.camera.cap.read()
+                
+                # Sending image data to external computer for processing
+                self.client.publish(topic='PiCarFrame', payload=str(bytearray(frame)))
+
+                # Catching user input
+                key = cv2.waitKey(1) & 0xFF
+
+                # Checking to exit program (with 'q' or 'esc')
+                if key == ord('q') or key == 27:
+                    self.stop()
+                    break
+
+                # Otherwise execute user command
+                self.__user_command(key)
+                if self.params['help']: self.__show_help(frame)
+
+                # Showing frame
+                cv2.imshow('PiCamera', frame)
+                
+                # Checking to save frame
+                if self.params['capture']:
+                    self.__save_frame('Assets', frame)
+                    self.params['capture'] = False
+
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+            self.stop()
+            
+    def stop(self):
+        # Stopping motors and sending final msg
+        self.__on_x_press()
+        
+        # Disconnecting from broker
+        #self.client.loop_stop()
+        #self.client.disconnect()
+        
+    def __sync_camera_steer(self):
+        # Find out the value of the steering angle (50-140)
+        steer_angle = self.driver.params['steer']
+        
+        # Adjust the camera pan accordingly (135-45)
+        if steer_angle == 90:
+            camera_angle = 85
+        elif steer_angle < 90:
+            camera_angle = 135 - (steer_angle-50)*1.2
+        else:
+            camera_angle = 85 - (steer_angle-90)*0.8
+            
+        self.camera.change_pan(camera_angle)
+        
+    def __save_frame(self, path, frame):
+        name = '_'.join([str(x) for x in time.localtime()[0:6]])
+        cv2.imwrite(f'{path}/{name}.jpg', frame)
+        
+    def __user_command(self, key):
+        # Motor controls
+        if key == ord('w') and not self.colission: self.__on_w_press()
+        elif key == ord('s'): self.__on_s_press()
+        elif key == ord('a'): self.__on_a_press()
+        elif key == ord('d'): self.__on_d_press()
+        elif key == ord('x'): self.__on_x_press()
+
+        # Camera controls
+        elif key == ord('i'): self.camera.increment_tilt(5)
+        elif key == ord('k'): self.camera.increment_tilt(-5)
+        elif key == ord('j'): self.camera.increment_pan(5)
+        elif key == ord('l'): self.camera.increment_pan(-5)
+        
+        # Help and car data
+        elif key == ord('h'): self.params['help'] = not self.params['help']
+        
+        # Save current frame
+        elif key == 32: self.params['capture'] = True
+        
+    def __on_w_press(self):
+        self.driver.increment_speed(10)
+        self.client.publish(topic='PiCarSpeed', payload=self.driver.params['speed'])
+        
+    def __on_s_press(self):
+        self.driver.increment_speed(-10)
+        self.client.publish(topic='PiCarSpeed', payload=self.driver.params['speed'])
+        
+    def __on_a_press(self):
+        self.driver.increment_steer(-10)
+        self.__sync_camera_steer()
+        self.client.publish(topic='PiCarSteer', payload=self.driver.params['steer'])
+        
+    def __on_d_press(self):
+        self.driver.increment_steer(10)
+        self.__sync_camera_steer()
+        self.client.publish(topic='PiCarSteer', payload=self.driver.params['steer'])
+        
+    def __on_x_press(self):
+        self.driver.stop()
+        self.client.publish(topic='PiCarSpeed', payload=self.driver.params['speed'])
+
+    def __show_help(self, frame):
+        text = ["help: h",
+                "emergency break: x",
+                "quit: q/esc",
+                "speed up/down: w/s",
+                "left/right: a/d",
+                "camera up/down: i/k",
+                "camera left/right: j/l",
+                "start or stop recording: c"]
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10,20)
+        fontScale = 0.5
+        fontColor = (0,0,255)
+        lineType = 2
+        linepos = 0
+
+        for line in text:
+            bottomLeftCornerOfText = (10,20+linepos*20)
+            cv2.putText(frame, line, bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+            linepos = linepos + 1
+
+
+if __name__ == "__main__":
+    try:
+        car = SmartCar()
+        car.debug_mode()
+        car.start()
+
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+        self.camera.cap.release()
+    
+    
+    
+    
+    
